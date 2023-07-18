efafcf6fdeb24930b2583d8637d631f852b97c7e
Jeffrey <112028948+tijeffrey@users.noreply.github.com>
2023-07-17 22:26:25 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const PORT = 1250;
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+let db;
+let MongoUserPassword = process.env.MongoUserPassword;
+
+MongoClient.connect(`${MongoUserPassword}`, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database');
+    db = client.db('Project-0');
+  })
+  .catch(error => console.error(error));
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (request, response) =>{
+    db.collection('fill-blanks').find().toArray()
+    .then(results => {
+        console.log(results)
+        response.render('index.ejs', {info: results})
+    })
+});
+
+app.post('/fill-blanks', (request, response) =>{
+    db.collection('fill-blanks').insertOne({fullQuote: request.body.quote.toLowerCase(), workName: request.body.work, answer: '', completed: false, stanzaAndLineNumbers: [request.body.stanzaNumber, request.body.poemLineNumber]})
+    .then(result =>{
+        response.redirect('/');
+    })
+    .catch(error => console.error(error))
+})
+
+//update the answer in Mongodb
+//check if answer is same as fullQuote
+    //if same, completed: true -- triggers strikethrough of question
+    //if not same, completed: false stays same. nothing happens
+
+app.put('/checkAnswer', (request, response) => {
+    console.log(request.body)
+    db.collection('fill-blanks').updateOne({stanzaAndLineNumbers: request.body.poemLine},{
+        $set: {
+            answer: request.body.newAnswer
+        }
+    },{
+        sort: {_id: -1},
+        upsert: false
+    })
+    .then(result => {
+        console.log('Marked Complete')
+        response.json('Marked Complete')
+    })
+    .catch(error => console.error(error))
+})
+
+app.listen(PORT, () =>{
+    console.log(`The server is now running on port ${PORT}`)
+})
