71c388a8674f83a8e2c7cfe4702b10fc7d3e44d5
Jeffrey <112028948+tijeffrey@users.noreply.github.com>
2023-07-18 21:43:57 -0400

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,31 @@
+const submitBtn = document.querySelectorAll('.submitButton');
+const playerAnswer = document.querySelectorAll('.playerAnswer');
+
+Array.from(submitBtn).forEach((element)=>{
+    element.addEventListener('click', checkAnswer)
+})
+
+async function checkAnswer(){
+    // const itemText = this.parentNode.parentNode.childNodes[5].innerText;
+    const itemText = this.parentNode.parentNode.childNodes[5].value;
+
+    // const itemText = document.querySelectorAll('span')
+
+    console.log(itemText)
+
+    try{
+        const response = await fetch('checkAnswer', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'poemLine': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
